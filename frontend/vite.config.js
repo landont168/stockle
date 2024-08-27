8f1c28b4b7cf28e7abb72a7320732e5831b688f9
Landon Trinh <111808279+landont168@users.noreply.github.com>
2024-08-26 23:15:50 -0400

    fix: load env variable in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import dotenv from 'dotenv'
-dotenv.config()
-const API_TARGET_URL = process.env.VITE_API_URL
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const API_TARGET_URL = env.VITE_API_URL
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['@mui/x-data-grid'],
-  },
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: API_TARGET_URL,
-        changeOrigin: true,
+  return {
+    plugins: [react()],
+    optimizeDeps: {
+      include: ['@mui/x-data-grid'],
+    },
+    server: {
+      host: true,
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: API_TARGET_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
